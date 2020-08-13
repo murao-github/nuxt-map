676b57a6da6e67463b4f1d6cb04801f52529d4a9
murao <engr.murao@gmail.com>
2020-08-13 02:53:35 +0000

    Remove vue2-google-maps from dependencies

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,11 +34,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: [
-    {
-      src:"~plugins/vue2-google-maps.js"
-    },
-  ],
+  plugins: [],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
@@ -70,23 +66,5 @@ export default {
    ** Build configuration
    ** See https://nuxtjs.org/api/configuration-build/
    */
-  build: {
-      extend(config, ctx) {
-      //add for vue2-google-maps
-      if (!ctx.isClient) {
-        // This instructs Webpack to include `vue2-google-maps`'s Vue files
-        // for server-side rendering
-        config.externals.splice(0, 0, function (context, request, callback) {
-          if (/^vue2-google-maps($|\/)/.test(request)) {
-          callback(null, false)
-          } else {
-            callback()
-          }
-        })
-      }
-    }
-  },
-  vendor: [
-    "vue2-google-maps"
-  ],
+  build: {},
 }
