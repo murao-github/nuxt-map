40422f8d48da39afa89f7b39cbcd8cf2c5aa2c98
murao <engr.murao@gmail.com>
2020-08-21 12:21:52 +0000

    Map component separated from index page

---
+++ components/Map.vue
@@ -0,0 +1,42 @@
+<template>
+  <div ref="googleMap" class="googleMap" />
+</template>
+
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+
+export default {
+  name: 'Map',
+  data() {
+    return {
+      google: null,
+      mapConfig: {
+        center: {
+          lat: 34.666252,
+          lng: 133.918104,
+        },
+        zoom: 13,
+      },
+    }
+  },
+  async mounted() {
+    this.google = await GoogleMapsApiLoader({
+      apiKey: process.env.API_KEY,
+    })
+    this.initializeMap()
+  },
+  methods: {
+    initializeMap() {
+      const map = new this.google.maps.Map(this.$refs.googleMap, this.mapConfig)
+      console.log(map)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.googleMap {
+  height: 100%;
+  width: 100%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,42 +4,11 @@
     <div class="body">
       <Sidebar />
       <div class="main">
-        <div ref="googleMap" class="googleMap" />
+        <Map />
       </div>
     </div>
   </div>
 </template>
-
-<script>
-import GoogleMapsApiLoader from 'google-maps-api-loader'
-
-export default {
-  name: 'Map',
-  data() {
-    return {
-      google: null,
-      mapConfig: {
-        center: {
-          lat: 35.68944,
-          lng: 139.69167,
-        },
-        zoom: 17,
-      },
-    }
-  },
-  async mounted() {
-    this.google = await GoogleMapsApiLoader({
-      apiKey: process.env.API_KEY,
-    })
-    this.initializeMap()
-  },
-  methods: {
-    initializeMap() {
-      this.google.maps.Map(this.$refs.googleMap, this.mapConfig)
-    },
-  },
-}
-</script>
 
 <style scoped>
 .contents {
@@ -55,9 +24,5 @@ export default {
 }
 .main {
   flex: 1;
-}
-.googleMap {
-  height: 100%;
-  width: 100%;
 }
 </style>
