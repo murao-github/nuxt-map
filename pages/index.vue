6f175a9cc38423455dbe5644a0a316084ba722cb
murao <engr.murao@gmail.com>
2020-09-21 11:37:05 +0000

    Fix index structure
    Add map to index

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,44 @@
   <div class="body">
     <Sidebar />
     <div class="main">
-      <Map />
+      <div ref="googleMap" class="googleMap" />
     </div>
   </div>
 </template>
+
+<script>
+import GoogleMapsApiLoader from 'google-maps-api-loader'
+
+export default {
+  data() {
+    return {
+      google: null,
+      map: null,
+      mapConfig: {
+        center: {
+          lat: 34.666252,
+          lng: 133.918104,
+        },
+        zoom: 13,
+      },
+    }
+  },
+  async mounted() {
+    const googleMapApi = await GoogleMapsApiLoader({
+      apiKey: process.env.API_KEY,
+    })
+    this.google = googleMapApi
+    this.initializeMap()
+  },
+  methods: {
+    initializeMap() {
+      const mapContainer = this.$refs.googleMap
+      this.map = new this.google.maps.Map(mapContainer, this.mapConfig)
+      console.log('Init map')
+    },
+  },
+}
+</script>
 
 <style scoped>
 .body {
@@ -17,5 +51,9 @@
 }
 .main {
   flex: 1;
+}
+.googleMap {
+  height: 100%;
+  width: 100%;
 }
 </style>
